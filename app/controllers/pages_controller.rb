e5fbcaa0bcd7278d7c5ebec364b0769f64a5e396
Metin Lamby <metin.lamby@outlook.com>
2023-07-13 15:54:04 +0200

    added new landing page navbar to app

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,19 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :problems, :solutions, :features, :feedback]
 
   def home
   end
+
+  def problems
+  end
+
+  def solutions
+  end
+
+  def features
+  end
+
+  def feedback
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+  get "problems", to: "pages#problems"
+  get "solutions", to: "pages#solutions"
+  get "features", to: "pages#features"
+  get "feedback", to: "pages#feedback"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
